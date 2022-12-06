c09aaa508ec6053b3e534c273bee9aaefc5b44fc
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-12-05 21:00:12 -0600

    Add DB connection

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const apiRouter = require("./src/routes");
 const { logErrors, errorHandler } = require("./src/middlewares/errorHandler");
 const config = require("./src/lib/config");
+const db = require("./src/lib/db");
 
-// app.use(express.json());
+app.use(express.json());
 apiRouter(app);
 
 app.use(logErrors);
@@ -15,6 +16,13 @@ app.get("/", (req, res) => {
 });
 
 // Ejecutando el servidor HTTP
-app.listen(config.app.port, () =>
-  console.log(`Esuchando peticiones HTTP en el puerto ${config.app.port}`)
-);
+app.listen(config.app.port, async () => {
+  console.log(`Esuchando peticiones HTTP en el puerto ${config.app.port}`);
+
+  try {
+    await db.connect();
+    console.log("DB is connected 🤠");
+  } catch (err) {
+    console.error("Connection refused:", err);
+  }
+});
---
+++ src/lib/db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const connect = () => {
+  return new Promise(async (resolve, reject) => {
+    const { user, password, host } = config.db;
+
+    mongoose.connect(
+      `mongodb+srv://${user}:${password}@${host}/?retryWrites=true&w=majority`
+    );
+
+    const db = mongoose.connection;
+
+    db.on("connected", () => {
+      console.log("Connection successful 😀");
+      resolve(mongoose);
+    });
+
+    db.on("error", (err) => {
+      console.error("Connection failed 😞", err);
+      reject(err);
+    });
+  });
+};
+
+module.exports = { connect };
